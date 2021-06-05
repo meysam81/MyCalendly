7f2cd75c3c87ff73648570ce0c2d95fec7ad5dc5
Meysam Azad <MeysamAzad81@gmail.com>
2021-06-05 20:02:11 +0430

    Add initial base script for every endpoint module ever to exist 🍱

---
+++ app/api/base.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter, Body, Path, Query
+from fastapi.exceptions import HTTPException
+
+__all__ = (
+    "APIRouter",
+    "Body",
+    "HTTPException",
+    "Query",
+    "Path",
+)
