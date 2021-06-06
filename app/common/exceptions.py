cee41a26c4bc55a2011f83de77f1a589dcf5a9f8
Meysam Azad <MeysamAzad81@gmail.com>
2021-06-06 19:03:45 +0430

    Add another exception & make the interface more robust 🍱

--- app/common/exceptions.py
+++ app/common/exceptions.py
@@ -8,6 +8,15 @@ class BaseException_(HTTPException, metaclass=abc.ABCMeta):
     def __init__(self, detail):
         self.detail = detail
 
+    @property
+    @abc.abstractmethod
+    def status_code(self):
+        pass
+
 
 class EntityNotFound(BaseException_):
     status_code = HTTPStatus.NOT_FOUND
+
+
+class BadTimeFrameProvided(BaseException_):
+    status_code = HTTPStatus.BAD_REQUEST
